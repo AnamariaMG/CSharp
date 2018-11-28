f9ce671158aeae434e7f0aef91e65d9088235a70
Ana <ana.anuta2008@yahoo.co.uk>
2018-11-28 21:31:02 +0200

    Add files via upload

---
+++ SumOfTwoInt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercise 2 ---- https://www.w3resource.com/
+            // Compute the sum of two given integers
+            
+            Console.Write("a = ");
+            int a = System.Int32.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            int b = System.Int32.Parse(Console.ReadLine());
+
+            int sum = a + b;
+
+            Console.WriteLine("a + b = {0}", sum);  
+        }
+    }
+}
