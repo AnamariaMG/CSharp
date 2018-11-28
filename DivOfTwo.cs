093395028db679389570c14d4863dbfacdb7d2cf
Ana <ana.anuta2008@yahoo.co.uk>
2018-11-28 21:34:15 +0200

    Add files via upload

---
+++ DivOfTwo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercise 3 ---- https://www.w3resource.com/
+            // Divide two numbers
+
+            //Console.WriteLine(18 / 4);
+
+            Console.Write("Give a = ");
+            int a = System.Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give b different from 0.");
+            Console.Write("b = ");
+            int b = System.Int32.Parse(Console.ReadLine());
+
+            try
+            {
+                int div = a / b;
+                Console.WriteLine("{0} / {1} = {2}", a, b, div);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e);
+            }         
+        }
+    }
+}
